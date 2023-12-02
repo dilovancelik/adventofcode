af8f8d0299431cf8766d4f6cc7587a8f7e854bc3
Dilovan Celik <dilovan@celikconsulting.com>
2023-12-02 22:02:34 +0100

    gitignore added

--- day02/target/debug/deps/day02-47af3a3fe0291bcb.d
+++
@@ -1,5 +0,0 @@
-/Users/dilovan/repos/adventofcode/day02/target/debug/deps/day02-47af3a3fe0291bcb.rmeta: src/main.rs
-
-/Users/dilovan/repos/adventofcode/day02/target/debug/deps/day02-47af3a3fe0291bcb.d: src/main.rs
-
-src/main.rs:
--- day02/target/debug/deps/day02-c12344a05f504491.d
+++
@@ -1,5 +0,0 @@
-/Users/dilovan/repos/adventofcode/day02/target/debug/deps/day02-c12344a05f504491.rmeta: src/main.rs
-
-/Users/dilovan/repos/adventofcode/day02/target/debug/deps/day02-c12344a05f504491.d: src/main.rs
-
-src/main.rs:
