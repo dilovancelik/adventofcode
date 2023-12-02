3116fa37373039019bfd4f185e22fbb699a21230
Dilovan Celik <dilovan@celikconsulting.com>
2023-12-02 22:00:27 +0100

    added input day02

---
+++ day02/target/debug/deps/day02-c12344a05f504491.d
@@ -0,0 +1,5 @@
+/Users/dilovan/repos/adventofcode/day02/target/debug/deps/day02-c12344a05f504491.rmeta: src/main.rs
+
+/Users/dilovan/repos/adventofcode/day02/target/debug/deps/day02-c12344a05f504491.d: src/main.rs
+
+src/main.rs:
